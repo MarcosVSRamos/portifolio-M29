420f845a37729a22e87739ced6c5791f6824d546
MarcosDevRamos <santosramosmarcosvinicius@gmail.com>
2025-01-31 19:16:57 -0400

    iniciando a construção dos componentes

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/marcosvsramos.png" />
+    <Titulo>Marcos Ramos</Titulo>
+  </aside>
+)
+
+export default Sidebar
---
+++ src/containers/Sobre/index.tsx
@@ -0,0 +1,15 @@
+import Titulo from '../../components/Titulo'
+
+const Sobre = () => (
+  <section>
+    <Titulo>Sobre mim</Titulo>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis placeat
+      necessitatibus blanditiis consectetur incidunt facilis ea quae? Asperiores
+      porro nesciunt nulla. Nesciunt repudiandae tempora commodi nulla saepe
+      similique, velit accusamus.
+    </p>
+  </section>
+)
+
+export default Sobre
