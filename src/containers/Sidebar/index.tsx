0f981e8fe97aa0a0365d6fbbe9de8dcf93e4ac7c
MarcosDevRamos <santosramosmarcosvinicius@gmail.com>
2025-02-02 08:34:52 -0400

    acrecentando componentes

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -3,7 +3,7 @@ import Titulo from '../../components/Titulo'
 const Sidebar = () => (
   <aside>
     <img src="https://github.com/marcosvsramos.png" />
-    <Titulo>Marcos Ramos</Titulo>
+    <Titulo fontSize={17}>Marcos Ramos</Titulo>
   </aside>
 )
 
--- src/containers/Sobre/index.tsx
+++ src/containers/Sobre/index.tsx
@@ -1,14 +1,15 @@
 import Titulo from '../../components/Titulo'
+import Paragrafo from '../../components/Paragrafo'
 
 const Sobre = () => (
   <section>
-    <Titulo>Sobre mim</Titulo>
-    <p>
+    <Titulo fontSize={16}>Sobre mim</Titulo>
+    <Paragrafo>
       Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis placeat
       necessitatibus blanditiis consectetur incidunt facilis ea quae? Asperiores
       porro nesciunt nulla. Nesciunt repudiandae tempora commodi nulla saepe
       similique, velit accusamus.
-    </p>
+    </Paragrafo>
   </section>
 )
 
