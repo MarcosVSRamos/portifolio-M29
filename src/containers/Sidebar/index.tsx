9ed60c27e0c7854526929e538cb8be9c26f05c44
MarcosDevRamos <santosramosmarcosvinicius@gmail.com>
2025-02-04 12:47:37 -0400

    estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,21 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+import { BotaoTema, Descricao, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/marcosvsramos.png" />
-    <Titulo fontSize={17}>Marcos Ramos</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={17}>Marcos Ramos</Titulo>
+      <Paragrafo tipo="secundaria" fontSize={16}>
+        marcosvsramos
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Estudante Full-Stack
+      </Descricao>
+      <BotaoTema>Trocar Tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
