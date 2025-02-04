cbf56b6a4f32fed141402189f0107a61247a3e64
MarcosDevRamos <santosramosmarcosvinicius@gmail.com>
2025-02-03 20:14:47 -0400

    finalizando a seçao "sobre mim"

--- src/containers/Sobre/index.tsx
+++ src/containers/Sobre/index.tsx
@@ -1,5 +1,6 @@
 import Titulo from '../../components/Titulo'
 import Paragrafo from '../../components/Paragrafo'
+import { GithubSecao } from './styles'
 
 const Sobre = () => (
   <section>
@@ -10,6 +11,10 @@ const Sobre = () => (
       porro nesciunt nulla. Nesciunt repudiandae tempora commodi nulla saepe
       similique, velit accusamus.
     </Paragrafo>
+    <GithubSecao>
+      <img src="https://github-readme-stats.vercel.app/api?username=marcosvsramos&show_icons=true&theme=dracula&include_all_commits=true&count_private=true" />
+      <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=marcosvsramos&layout=compact&langs_count=7&theme=dracula" />
+    </GithubSecao>
   </section>
 )
 
